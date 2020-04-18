f7200b1b0631910529701568a38a6fb153e59e08
Josef Ahmad <josef.ahmad@mongodb.com>
2020-04-18 13:55:08 +0000

    add initial boilerplate for $in

--- main.c
+++ main.c
@@ -12,67 +12,30 @@ main (int argc, char *argv[])
    bson_error_t error;
    char *str;
    bool retval;
+   bson_t *query;
+   const bson_t *doc;
 
-   /*
-    * Required to initialize libmongoc's internals
-    */
    mongoc_init ();
-
-   /*
-    * Optionally get MongoDB URI from command line
-    */
-   if (argc > 1) {
-      uri_str = argv[1];
-   }
-
-   /*
-    * Create a new client instance
-    */
    client = mongoc_client_new (uri_str);
-
-   /*
-    * Register the application name so we can track it in the profile logs
-    * on the server. This can also be done from the URI (see other examples).
-    */
    mongoc_client_set_appname (client, "connect-example");
-
-   /*
-    * Get a handle on the database "db_name" and collection "coll_name"
-    */
    database = mongoc_client_get_database (client, "db_name");
    collection = mongoc_client_get_collection (client, "db_name", "coll_name");
 
-   /*
-    * Do work. This example pings the database, prints the result as JSON and
-    * performs an insert
-    */
-   command = BCON_NEW ("ping", BCON_INT32 (1));
-
-   retval = mongoc_client_command_simple (
-      client, "admin", command, NULL, &reply, &error);
-
-   if (!retval) {
-      fprintf (stderr, "%s\n", error.message);
-      return EXIT_FAILURE;
+   mongoc_cursor_t *cursor;
+   query = (bson_t *)malloc(1024); //XXX
+   retval = bson_init_from_json(query,
+                    "{ \"a\": { \"$in\": [0, 1, 2 ] } }",
+                    -1,
+                    NULL);
+   cursor = mongoc_collection_find_with_opts (collection, query, NULL, NULL);
+   while (mongoc_cursor_next (cursor, &doc)) {
+      str = bson_as_canonical_extended_json (doc, NULL);
+      printf ("%s\n", str);
+      bson_free (str);
    }
 
-   str = bson_as_json (&reply, NULL);
-   printf ("%s\n", str);
-
-   insert = BCON_NEW ("hello", BCON_UTF8 ("world"));
-
-   if (!mongoc_collection_insert_one (collection, insert, NULL, NULL, &error)) {
-      fprintf (stderr, "%s\n", error.message);
-   }
-
-   bson_destroy (insert);
-   bson_destroy (&reply);
-   bson_destroy (command);
-   bson_free (str);
-
-   /*
-    * Release our handles and clean up libmongoc
-    */
+   bson_destroy (query);
+   mongoc_cursor_destroy (cursor);
    mongoc_collection_destroy (collection);
    mongoc_database_destroy (database);
    mongoc_client_destroy (client);
